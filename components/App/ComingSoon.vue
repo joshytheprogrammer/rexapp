70be4f67002101f49a8eb85f3a1a3c57bf9d965d
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-08-30 11:28:01 +0100

    made changes: ''

---
+++ components/App/ComingSoon.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-screen bg-gray-800">
+    <h1 class="text-4xl font-semibold text-white mb-4">Coming Soon</h1>
+    <p class="text-gray-400 text-lg mb-8">We're working hard to bring you something amazing!</p>
+    <div class="flex space-x-4">
+      <div class="bg-white rounded-full p-2">
+        <Icon class="cursor-pointer text-blue-500 transition-all duration-300" name="mdi:twitter" size="1.55em" />
+      </div>
+      <div class="bg-white rounded-full p-2">
+        <Icon class="cursor-pointer text-blue-600 transition-all duration-300" name="mdi:facebook" size="1.55em" />
+      </div>
+      <div class="bg-white rounded-full p-2">
+        <Icon class="cursor-pointer text-pink-500 transition-all duration-300" name="mdi:instagram" size="1.55em" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
