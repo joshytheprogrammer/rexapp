8f17c2265b38d3381d5e82c3961c2ca8185d5b3e
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-08-30 15:12:47 +0100

    made changes: ''

--- components/App/Footer.vue
+++ components/App/Footer.vue
@@ -11,76 +11,86 @@
           <button type="submit" class="flex-none rounded-r-md bg-green-800 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Subscribe</button>
         </div>
     </div>
-    <div class="flex justify-between gap-4 py-8 text-white">
-      <div class="block basis-1/2 ">
+    <div class="block mx-auto justify-center lg:flex justify-between gap-4 py-8 text-white border-b border-gray-800">
+      <div class="flex flex-col justify-between basis-1/2 text-center lg:text-left lg:pr-8 pb-8 lg:pb-0">
         <h3 class="text-xl font-semibold leading-9 tracking-tight">ABOUT US</h3>
-        <p class=" text-md leading-8 text-gray-300">The RexApp project is driven by the mission to revolutionize the way automobile spare parts are procured in Nigeria. Through the creation of an intuitive and accessible web application, our goal is to streamline the entire process, making it easier and more convenient for individuals and businesses to source the necessary components for their vehicles.</p>
-        <div class="">
-
-        </div>
+        <p class="text-center lg:text-left text-md leading-8 text-gray-300">The RexApp project is driven by the mission to revolutionize the way automobile spare parts are procured in Nigeria. Through the creation of an intuitive and accessible web application, our goal is to streamline the entire process, making it easier and more convenient for individuals and businesses to source the necessary components for their vehicles.</p>
       </div>
-      <div class="flex justify-between basis-1/2 gap-4">
-        <div class="basis-1/3">
+      <div class="md:flex-nowrap text-center flex justify-between basis-1/2 gap-2 lg:text-left lg:gap-4">
+        <div class="md:basis-1/3">
           <h3 class="text-xl font-semibold leading-9 tracking-tight">MY ACCOUNT</h3>
-          <ul class="">
-            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300 font-medium">
+          <ul class="text-sm md:text-base font-normal md:font-medium">
+            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300">
               <NuxtLink to="/account/my">My Profile</NuxtLink>
             </li>
-            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300 font-medium">
+            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300">
               <NuxtLink to="/cart">My Cart</NuxtLink>
             </li>
-            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300 font-medium">
+            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300">
               <NuxtLink to="/account/my?page=wishlist">My Wishlist</NuxtLink>
             </li>
-            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300 font-medium">
+            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300">
               <NuxtLink to="/account/auth">Login</NuxtLink>
             </li>
-            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300 font-medium">
+            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300">
               <NuxtLink to="/account/auth">Register</NuxtLink>
             </li>
           </ul>
         </div>
-        <div class="basis-1/3">
+        <div class="md:basis-1/3">
           <h3 class="text-xl font-semibold leading-9 tracking-tight">PAGE RESOURCES</h3>
-          <ul class="">
-            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300 font-medium">
+          <ul class="text-sm md:text-base font-normal md:font-medium">
+            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300">
               <NuxtLink to="/contact">Contact Us</NuxtLink>
             </li>
-            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300 font-medium">
+            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300">
               <NuxtLink to="/about">About Us</NuxtLink>
             </li>
-            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300 font-medium">
+            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300">
               <NuxtLink to="/orders">Track Orders</NuxtLink>
             </li>
-            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300 font-medium">
+            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300">
               <NuxtLink to="/about/privacy">Privacy Policy</NuxtLink>
             </li>
-            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300 font-medium">
+            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300">
               <NuxtLink to="/account/auth">Register</NuxtLink>
             </li>
           </ul>
         </div>
-        <div class="basis-1/3">
+        <div class="md:basis-1/3">
           <h3 class="text-xl font-semibold leading-9 tracking-tight">FIND BY CATEGORY</h3>
-          <ul class="">
-            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300 font-medium">
+          <ul class="text-sm md:text-base font-normal md:font-medium">
+            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300">
               <NuxtLink to="/category/">Engine Parts</NuxtLink>
             </li>
-            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300 font-medium">
+            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300">
               <NuxtLink to="/category">Brake System</NuxtLink>
             </li>
-            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300 font-medium">
+            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300">
               <NuxtLink to="/category/">Electrical System</NuxtLink>
             </li>
-            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300 font-medium">
+            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300">
               <NuxtLink to="/category/">Exhaust System</NuxtLink>
             </li>
-            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300 font-medium">
+            <li class="cursor-pointer py-2 hover:text-green-800 transition-all duration-300">
               <NuxtLink to="/category/">Fuel System</NuxtLink>
             </li>
           </ul>
         </div>
       </div>
+    </div>
+    <div class="flex flex-wrap justify-center text-center md:text-left md:justify-between items-center py-8 text-white">
+      <div class="w-fit">
+        <NuxtLink class="flex items-center space-around no-underline" to="/">
+          <!-- <img src="" alt=""> -->
+          <Icon class="text-green-800" size="4em" name="ph:car-fill" />
+          <h1 class="font-bold text-xl md:text-2xl lg:text-4xl italic px-2">Rex<span class="text-green-800">app</span></h1>
+        </NuxtLink>
+      </div>
+      <p class="py-4 md:py-0">
+        &copy; {{ new Date().getFullYear() }} WallsandGates Limited. All Rights Reserved
+      </p>
+      <img class="max-w-xs" src="https://res.cloudinary.com/dsgvwxygr/image/upload/v1693404461/rexapp/credit__cards_y6z2yy.png" alt="paywment_providers">
     </div>
   </div>
 </template>
