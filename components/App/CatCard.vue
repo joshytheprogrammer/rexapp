82090143681bfcb27d24446bceac26236e8ff402
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-09-04 15:50:17 +0100

    made changes: ''

---
+++ components/App/CatCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="border px-6 py-8 w-fit rounded-md text-center hover:bg-gray-100 hover:text-green-800 cursor-pointer">
+    <img src="https://risingtheme.com/html/demo-partsix/partsix/assets/img/product/main-product/product1.webp" alt="product1" class="w-full max-h-48 object-cover md:h-auto">
+    <NuxtLink to="/categories/" class="pt-4 font-semibold text-base text-inherit">Exhaust Categories</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
