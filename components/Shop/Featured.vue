f452b9487f70ab5ab05d283c2cf83375aa15f595
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-09-07 12:24:24 +0100

    made changes: ''

---
+++ components/Shop/Featured.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="">
+    <ShopHeader title="Recently Uploaded Products" />
+    <div class="flex justify-start">  
+      <AppProductLVCard v-if="pending" />
+      <AppProductVCard v-for="p in data.products" :key="p._id" :p="p" v-else />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { data, pending, error } = await useFetch('/products/recent?limit=6', {
+  baseURL: useRuntimeConfig().public.baseURL,
+  immediate: true,
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
