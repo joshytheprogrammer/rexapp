02512172b776e742dcb9699eaf29cc216ac90e5c
Joshua <jurassicjoshua@protonmail.com>
2023-09-15 13:11:17 +0100

    made changes: 'fixed minor bugs'

--- components/Search/PCategories.vue
+++ components/Search/PCategories.vue
@@ -2,7 +2,7 @@
   <div class="bg-transparent w-full p-4 shadow-md rounded-lg mb-4 transition-all duration-300">
     <span class="text-black font-semibold text-base ">Popular Categories</span>
     <span class="block py-2"></span>
-    <NuxtLink class="block my-1" v-for="item in categories" :key="item._id" :to="'/categories/'+item.slug">
+    <NuxtLink class="block my-1" v-for="item in data.categories" :key="item._id" :to="'/categories/'+item.slug">
       <div class="flex justify-start cursor-pointer items-center p-2 border hover:border-green-800 rounded-md">
         <img :src="item.imageURL" class="w-12 object-cover md:h-auto">
         <span> {{ item.name }}</span>
@@ -15,6 +15,4 @@
 const { data, error } = await useFetch('/categories/all?fields=_id,name,slug,imageURL&sort=name&limit=5', {
   baseURL: useRuntimeConfig().public.baseURL,
 });
-
-const categories = data.value?.categories;
 </script>
