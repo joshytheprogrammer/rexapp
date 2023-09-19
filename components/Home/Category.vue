f7aa8e418fcbf73f0c7d8e8d2936291c66eb1431
Joshua <jurassicjoshua@protonmail.com>
2023-09-19 10:24:59 +0100

    made changes: 'updated header'

--- components/Home/Category.vue
+++ components/Home/Category.vue
@@ -1,7 +1,7 @@
 <template>
   <div class=" px-4 sm:px-8 md:px-8 lg:px-18 py-4">
     <ShopHeader title="Trending Categories" />
-    <div v-for="p in data.categories" :key="p._id"  class="lg:grid md:grid-cols-3 lg:grid-cols-5 gap-2">
+    <div v-for="p in data.categories" :key="p._id"  class="lg:grid md:grid-cols-3 lg:grid-cols-5 gap-4">
       <AppCategoryCard class="mx-auto my-4" :p="p" />
     </div>
   </div>
--- components/Shop/Header.vue
+++ components/Shop/Header.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="bg-gray-100 my-4 px-8 py-4 rounded-md text-center md:text-left">
+  <div class="flex items-center gap-4 my-4py-4 rounded-md text-center md:text-left">
+    <div class="w-fit bg-transparent border border-green-800 rounded-full p-2">
+      <div class="bg-green-800 rounded-full p-1"></div>
+    </div>
     <h2 class=" text-black text-2xl font-medium">{{ title }}</h2>
   </div>
 </template>
@@ -7,7 +10,3 @@
 <script setup>
 defineProps(['title']);
 </script>
-
-<style lang="scss" scoped>
-
-</style>
