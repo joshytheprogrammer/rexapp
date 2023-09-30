eba9167251f489b25a790f39de0e590cf0923c0b
Joshua <jurassicjoshua@protonmail.com>
2023-09-30 10:23:24 +0100

    made changes requested

--- components/Home/Jumbotron.vue
+++ components/Home/Jumbotron.vue
@@ -2,8 +2,8 @@
   <div class="hero w-full bg-green-200 text-white lg:text-black bg-scroll bg-clip-border bg-no-repeat bg-right-bottom px-4 sm:px-8 md:px-8 lg:px-18 py-36 uppercase text-center md:text-left flex items-center md:items-start flex-col justify-center">
     <div class="text-xl space-y-8">
       <p class="font-semibold ">best price</p>
-      <p class="text-4xl lg:text-6xl text-green-800 font-bold">Everything</p>
-      <p class="capitalize">High Quality | Extreme Performance</p>
+      <p class="text-4xl lg:text-6xl text-green-800 font-bold">Great Deal</p>
+      <p class="capitalize">On Every Product</p>
       <p ><NuxtLink class="block mx-auto md:mx-0 bg-green-800 rounded-full border border-green-800 text-white w-fit px-8 py-4" to="/shop" >Shop Now</NuxtLink></p>
     </div>
   </div>
