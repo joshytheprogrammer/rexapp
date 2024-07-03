2722f6b8d0611044dc4df0fd3856747cab192429
joshytheprogrammer <jurassicjoshua@protonmail.com>
2024-07-03 12:38:37 +0100

    made changes: ''

---
+++ components/Order/Notes.vue
@@ -0,0 +1,39 @@
+<template>
+ <div class="w-fit block mb-4 rounded-md shadow-lg border bg-white px-4 py-8 lg:py-10 space-y-2 text-sm font-medium">
+  <h2 class="text-xl lg:text-2xl font-normal mb-4">Order Notes</h2>
+  <p class="space-y-4 text-gray-600">
+    <p>
+      Dear Customer,
+    </p>
+
+    <p>
+      We have dispatched an email to the email address you provided, containing the current price quotation for the products listed in your order summary. Within the email, you will find a convenient payment link. Once your payment is successfully processed, your selected items will be promptly dispatched to the address you have previously provided.
+    </p>
+
+    <p>
+      Upon your receipt of the product, please take a moment to confirm its safe arrival. Subsequently, we will promptly update the order status to reflect its successful delivery.
+    </p>
+
+    <p>
+      Should you wish to cancel your order for any reason, please do not hesitate to reach out to our dedicated support team at <NuxtLink to="mailto:support@rexapp.com" class="underline text-green-800">support@rexapp.com</NuxtLink>. We are here to assist you with any inquiries or requests you may have.
+    </p>
+
+    <p>
+      Thank you for choosing RexApp for your shopping needs.
+    </p>
+
+    <p >
+      Best Regards,<br>
+      RexApp Team
+    </p>
+  </p>
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/about/terms.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Terms
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
